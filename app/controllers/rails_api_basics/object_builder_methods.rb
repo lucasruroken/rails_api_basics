80572a2705517da327f852f076601a61349040eb
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2019-03-04 12:01:09 -0300

    Minor changes

---
+++ app/controllers/rails_api_basics/object_builder_methods.rb
@@ -0,0 +1,57 @@
+module RailsApiBasics
+  module ObjectBuilderMethods
+    def klass
+      @klass ||= self.class.name.demodulize.underscore.gsub("_controller", "")
+    end
+
+    def serializer
+      "#{klass.singularize.camelize}Serializer".constantize
+    end
+
+    def scope
+      @scope ||= klass.singularize.camelize.constantize
+    end
+
+    def find_object
+      @object = scope.find(params[:id])
+    end
+
+    def object_params
+      raise "Should implement in inherited class"
+    end
+
+    # Methods per action
+    # Index
+    def scope_index
+      scope.latest
+    end
+
+    def serializer_index_includes
+      nil
+    end
+
+    # Create
+    def scope_create
+      scope
+    end
+
+    # Show
+    def object_show
+      @object
+    end
+
+    def seralizer_show_includes
+      nil
+    end
+
+    # Update
+    def object_update
+      @bject
+    end
+
+    # Destroy
+    def object_destroy
+      @object
+    end
+  end
+end
--- app/controllers/rails_api_basics/resources.rb
+++ app/controllers/rails_api_basics/resources.rb
@@ -1,14 +1,16 @@
 module RailsApiBasics
   class Resources < BaseController
+    include ObjectBuilderMethods
+
     before_action :find_object, only: [:show, :update, :destroy]
 
     def index
-      @objects = scope.latest.page(params[:page])
-      render json: serializer.new(@objects).serialized_json
+      @objects = scope_index.page(params[:page])
+      render json: serializer.new(@objects, serializer_index_includes).serialized_json
     end
 
     def create
-      @object = scope.new(object_params)
+      @object = scope_create.new(object_params)
 
       if @object.save
         render status: :created
@@ -18,11 +20,11 @@ module RailsApiBasics
     end
 
     def show
-      render json: serializer.new(@object).serialized_json
+      render json: serializer.new(@object, seralizer_show_includes).serialized_json
     end
 
     def update
-      if @object.update(object_params)
+      if object_update.update(object_params)
         render status: :no_content
       else
         render json: { errors: @object.errors.full_messages }, status: :unprocessable_entity
@@ -30,30 +32,8 @@ module RailsApiBasics
     end
 
     def destroy
-      @object.destroy
+      object_destroy.destroy
       render status: :no_content
-    end
-
-    private
-
-    def klass
-      @klass ||= self.class.name.demodulize.underscore.gsub("_controller", "")
-    end
-
-    def serializer
-      "#{klass.singularize.camelize}Serializer".constantize
-    end
-
-    def scope
-      @scope ||= klass.singularize.camelize.constantize
-    end
-
-    def find_object
-      @object = scope.find(params[:id])
-    end
-
-    def object_params
-      raise "Should implement in inherited class"
     end
   end
 end
